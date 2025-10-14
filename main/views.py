ba0ff5f3c7d31181b633dca44eb241d2457a190d
tazievvadim07-wq <tazievvadim07@gmail.com>
2025-10-14 17:10:14 +0500

    контекст в шаблонах

--- main/views.py
+++ main/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'main/home.html')
+def students_view(request):
+    students = [
+        {"name": "Айжан", "age": 19, "group": "IT-23A"},
+        {"name": "Мансур", "age": 20, "group": "IT-23A"},
+        {"name": "Алихан", "age": 18, "group": "IT-23B"},
+    ]
+
+    group_info = {"name": "IT-23A", "specialty": "Веб-разработка", "year": 2025}
+
+    context = {
+        "students": students,
+        "group_info": group_info
+    }
+
+    return render(request, "main/students.html", context)

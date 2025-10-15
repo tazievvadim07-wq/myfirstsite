ec0d7944acd77b3bf849246660e67c8607c3cd78
tazievvadim07-wq <tazievvadim07@gmail.com>
2025-10-15 16:12:12 +0500

    Prepare project for GitHub

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("students/", views.students_view, name="students"),
    path('feedback/', views.feedback, name='feedback'),
+    path('articles/<int:id>/', views.article_detail, name="articles"),
+    path('user/<str:id>/', views.user_detail, name="user"),
+    path('article/<int:id>/', views.article_detail_1, name="article1"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def students_view(request):
     students = [
@@ -27,3 +28,23 @@ def feedback(request):
            result = f"Спасибо, {name}! Мы получили твоё сообщение: {message}"
 
    return render(request, "main/form.html", {"result": result})
+
+
+def article_detail(request, id):
+   return HttpResponse(f"Вы просматриваете статью с ID = {id}")
+
+def user_detail(request, id):
+    return HttpResponse (f"Профиль пользователя {id}")
+
+articles = [
+   {"id": 1, "title": "Django основы"},
+   {"id": 2, "title": "Шаблоны в Django"},
+   {"id": 3, "title": "ORM и базы данных"}
+]
+
+def article_detail_1(request, id):
+   article = next((a for a in articles if a["id"] == id), None)
+   if article:
+       return HttpResponse(f"Статья: {article['title']}")
+   else:
+       return HttpResponse("Статья не найдена")

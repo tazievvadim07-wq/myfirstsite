f53a6fbd00b079487f8be686ebcaef0ee69aa730
tazievvadim07-wq <tazievvadim07@gmail.com>
2025-10-21 15:21:02 +0500

    создание админки

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Article, Category
 
-# Register your models here.
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "created_at")
+    list_filter = ("category",)
+    search_fields = ("title", "content")
+
+admin.site.register(Category)
+
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название категории")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+class Article(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="Содержание")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+
+    def __str__(self):
+        return self.title
